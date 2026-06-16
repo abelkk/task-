d64381a79b4c18cdc1e0097f1c6b8285a1d85b73
abelkk <abelkemb@gmail.com>
2026-06-16 16:35:01 +0300

    updated readme

--- task.py
+++ task.py
@@ -1,10 +1,11 @@
 class_ages = (20,50,34,39,45)
 def total_ages():
-  print(20+50+34+39+45)
+   total = sum(class_ages)
+   print(total)
 
-def max_age():
-    highest_age = max(class_ages)
-    print(highest_age)
+def min_age():
+    lowest_age = min(class_ages)
+    print(lowest_age)
 
 total_ages()
-max_age()
+min_age()
